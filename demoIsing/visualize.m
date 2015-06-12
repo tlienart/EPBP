eda2db730eb059200fdd6a9772e19fdd58335380
Thibaut Lienart <tibMBA@tibMBA.local>
2015-06-12 11:07:58 +0100

    testingPBP

--- demoIsing/visualize.m
+++ demoIsing/visualize.m
@@ -1,6 +1,6 @@
 expname = 'demoIsing';
 Ngrid   = num2str(200);
-Np 		= num2str(100);
+Np 		= num2str(50);
 Nc 		= num2str(10);
 runn    = num2str(1);
 lbpd_grid    = load([expname,'_lbpd_grid_np',Ngrid,'.dat']);
@@ -9,6 +9,9 @@ lbpd_origbel = load([expname,'_lbpd_origbel_np',Ngrid,'.dat']);
 %
 epbp_estbel  = load([expname,'_epbp_est_beliefs_np',Np,'_run',runn,'.dat']);
 fepbp_estbel = load([expname,'_fepbp_est_beliefs_np',Np,'_nc',Nc,'_run',runn,'.dat']);
+%
+Np = num2str(50);
+pbp_estbel   = load([expname,'_pbp_est_beliefs_np',Np,'_run',runn,'.dat']);
 
 figure
 node = 1;
@@ -18,6 +21,7 @@ plot(lbpd_grid,lbpd_beliefs(node,:)/trapz(lbpd_grid,lbpd_beliefs(node,:)),'-b')
 plot(lbpd_grid,lbpd_origbel(node,:)/trapz(lbpd_grid,lbpd_origbel(node,:)),'--r')
 plot(lbpd_grid,epbp_estbel(node,:)/trapz(lbpd_grid,epbp_estbel(node,:)),'--','color','DarkGreen')
 plot(lbpd_grid,fepbp_estbel(node,:)/trapz(lbpd_grid,fepbp_estbel(node,:)),'--','color','Cornflowerblue')
+plot(lbpd_grid,pbp_estbel(node,:)/trapz(lbpd_grid,pbp_estbel(node,:)),'--','color','DarkSalmon')
 figure
 node = 8;
 title('Node 7')
@@ -26,6 +30,7 @@ plot(lbpd_grid,lbpd_beliefs(node,:)/trapz(lbpd_grid,lbpd_beliefs(node,:)),'-b')
 plot(lbpd_grid,lbpd_origbel(node,:)/trapz(lbpd_grid,lbpd_origbel(node,:)),'--r')
 plot(lbpd_grid,epbp_estbel(node,:)/trapz(lbpd_grid,epbp_estbel(node,:)),'--','color','DarkGreen')
 plot(lbpd_grid,fepbp_estbel(node,:)/trapz(lbpd_grid,fepbp_estbel(node,:)),'--','color','Cornflowerblue')
+plot(lbpd_grid,pbp_estbel(node,:)/trapz(lbpd_grid,pbp_estbel(node,:)),'--','color','DarkSalmon')
 figure
 node = 13;
 title('Node 16')
@@ -34,3 +39,4 @@ plot(lbpd_grid,lbpd_beliefs(node,:)/trapz(lbpd_grid,lbpd_beliefs(node,:)),'-b')
 plot(lbpd_grid,lbpd_origbel(node,:)/trapz(lbpd_grid,lbpd_origbel(node,:)),'--r')
 plot(lbpd_grid,epbp_estbel(node,:)/trapz(lbpd_grid,epbp_estbel(node,:)),'--','color','DarkGreen')
 plot(lbpd_grid,fepbp_estbel(node,:)/trapz(lbpd_grid,fepbp_estbel(node,:)),'--','color','Cornflowerblue')
+plot(lbpd_grid,pbp_estbel(node,:)/trapz(lbpd_grid,pbp_estbel(node,:)),'--','color','DarkSalmon')
