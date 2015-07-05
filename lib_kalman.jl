e54e51b26cc7b826c6c1203ee6c5b5fd37e993fa
Thibaut Lienart <ltib@me.com>
2015-07-05 11:01:56 +0100

    Delete lib_kalman.jl

--- lib_kalman.jl
+++
@@ -1,5 +0,0 @@
-#
-# 	Code released under the MIT license, see corresponding LICENSE file
-#	(c) 2015, Thibaut Lienart
-#
-# --------------------------------------------------------------------------------------------------
